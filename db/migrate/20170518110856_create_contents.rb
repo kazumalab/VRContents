b1f5367b530748ff04c1b8ea0f9c5f9aedaadfd2
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-05-18 20:21:47 +0900

    first commit.

---
+++ db/migrate/20170518110856_create_contents.rb
@@ -0,0 +1,13 @@
+class CreateContents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :contents do |t|
+      t.string :title
+      t.text :description
+      t.string :telephone
+      t.string :email
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
