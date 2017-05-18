bfe12237bbb0ebde2e2c91aae0e186770346de32
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-05-18 23:07:29 +0900

    rewrite contents helper to  application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def self.markdown(text)
+    options = {
+        autolink: true,
+        space_after_headers: true,
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        tables: true,
+        hard_wrap: true,
+        xhtml: true,
+        lax_html_blocks: true,
+        strikethrough: true
+    }
+    renderer = Redcarpet::Render::HTML.new
+    markdown = Redcarpet::Markdown.new(renderer, options)
+    markdown.render(text)
+  end
+
+  def self.unmarkdown(text)
+    text.delete("#()[]").truncate(60)
+  end
 end
